e2de19b064107e4017e0deba995cacdaa5624c94
Safari Knight <safariknight2@gmail.com>
2024-03-09 14:39:31 +0200

    updated challenge 4

--- ps2/s1/chal4.js
+++ ps2/s1/chal4.js
@@ -1,1 +1,12 @@
+let languages = ["English", "Arabic", "French", "German"]
+
+
+languages[2] = "Spanish"
+let newLength = languages.push("French") // Push adds an element and returns the new length of the array
+
+console.log(`I speak ${newLength} languages they are: `)
+languages.forEach((i) => { // For loop
+  console.log(i)
+})
+
 
