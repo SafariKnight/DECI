81542730cb91d69ffdedcb96cdb12242a3eafddc
Kareem Hisham <safariknight2@gmail.com>
2024-11-23 10:00:28 +0200

    addition: level 4 week 2 activity

--- level4/week2/index.js
+++ level4/week2/index.js
@@ -1,1 +1,24 @@
-console.log("Hello, World!")
+function createGame(min, max) {
+  let secret = min + Math.floor(Math.random() * (max + 1));   
+  function guessNumber(guess) {
+    switch (true) {
+      case guess > secret:
+        return { message: "Too High!", correct: false };
+      case guess < secret:
+        return { message: "Too Low!", correct: false };
+      case guess === secret:
+        return { message: "Correct!", correct: true };
+      default:
+        return { message: "Invalid Input!", correct: false };
+    }
+  }
+  return guessNumber;
+}
+
+const guessNumber = createGame(0, 10);
+
+let won = false;
+let message = "Guess a Number";
+while (!won){
+  ({ message: message, correct: won } = guessNumber(parseInt(prompt(message))));
+}
