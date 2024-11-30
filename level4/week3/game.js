2b5e205656874c5a5c958cf009cd90e6a47189b2
Kareem Hisham <safariknight2@gmail.com>
2024-11-30 10:15:05 +0200

    change: level 4 week 3

--- level4/week3/game.js
+++ level4/week3/game.js
@@ -1,31 +1,61 @@
+function getRandomModifier() {
+    const maxModifier = 100;
+    const minModifier = 85;
+    return Math.floor(Math.random() * (maxModifier - minModifier) + minModifier) / 100;
+}
+
 class Character {
-  constructor(name, health, attack) {
+  constructor(name, health, attack, mana) {
+    if (mana <= 0) return;
     this.name = name
     this.health = health
-    this.attack = attack
+    this.maxHealth = health;
+    this.attack = attack;
+    this.mana = mana;
   }
   decreaseHealth(damage) {
     this.health -= damage;
     this.health = (Math.round(this.health * 10)) / 10
+    if (this.health < 0) this.health = 0;
+  }
+  heal() {
+    if (this.mana < 0) return;
+    const randomModifier = getRandomModifier();
+    const amountToHeal = 0.2 * this.mana * randomModifier; 
+    this.health += amountToHeal;
+    this.mana -= 0.2 * this.mana;
+    this.health = (Math.round(this.health * 10)) / 10
   }
   attackEnemy(enemy) {
-    const maxModifier = 100;
-    const minModifier = 85;
-    const randomModifier = Math.floor(Math.random() * (maxModifier - minModifier) + minModifier)
-    enemy.decreaseHealth((this.attack * (randomModifier / 100)));
+    const randomModifier = getRandomModifier();
+    enemy.decreaseHealth(this.attack * randomModifier);
   }
+}
+
+function turnAction(user, target) {
+  const decision = Math.random();
+  console.log(`${user.name}'s Turn!`)
+
+  if (decision > 0.6) {
+    user.heal();
+    console.log(`\t${user.name} Healed!`)
+  }
+  else {
+    user.attackEnemy(target);
+    console.log(`\t${user.name} attacked ${target.name}!`)
+  };
 }
 
 function startFight(player, monster) {
   while (player.health > 0 && monster.health > 0) {
-    if (player.health > 0) {
-      player.attackEnemy(monster)
-      console.log(`Monster Health: ${monster.health}`)
-    }
-    if (monster.health > 0) {
-      monster.attackEnemy(player)
-      console.log(`Player Health: ${player.health}`)
-    }
+    if (player.health > 0) turnAction(player, monster);
+    if (monster.health > 0) turnAction(monster, player);
+
+
+    console.log("\n")
+    console.log(`${player.name} Health: ${player.health}`)
+    console.log(`${monster.name} Health: ${monster.health}`)
+    console.log("--------------------------------------")
   }
 
   if (player.health > 0) {
@@ -35,9 +65,7 @@ function startFight(player, monster) {
   console.log(`${monster.name} won!`)
 }
 
-const player = new Character("Player", 100, 10);
-const monster = new Character("Monster", 100, 15);
+const player = new Character("Player", 100, 15, 100);
+const monster = new Character("Monster", 100, 15, 100);
 
 startFight(player, monster);
-
-console.log(monster.health);
