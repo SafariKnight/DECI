ab87c6d35edbeadabbc2408929c85f17ccd0b391
Kareem Hisham <safariknight2@gmail.com>
2024-12-21 10:18:57 +0200

    addition: level 4 week 6 activity

---
+++ level4/week6/dice.js
@@ -0,0 +1,39 @@
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+function rollDice() {
+  return new Promise((resolve) => {
+    setTimeout(
+      () => {
+        resolve(randomNum(1, 6));
+      },
+      randomNum(1000, 1000),
+    );
+  });
+}
+
+function playGame(targetNumber) {
+  return new Promise((resolve, reject) => {
+    rollDice()
+      .then((roll) => targetNumber === roll)
+      .then((guess) => {
+        console.log(guess);
+        if (guess === true) {
+          resolve("You Won!");
+        } else {
+          reject("You Lost!");
+        }
+      });
+  });
+}
+
+for (let i = 3; i > 0; i--) {
+  playGame(4)
+    .then((message) => {
+      console.log(message);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
---
+++ level4/week6/pokemon.js
@@ -0,0 +1,34 @@
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+function fetchRandomPokemonData(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then((response) => response.json())
+      .then((pokemon) => {
+        resolve({
+          name: pokemon.species.name,
+          height: pokemon.height,
+          weight: pokemon.weight,
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+function displayPokemonData() {
+  fetchRandomPokemonData(randomNum(1, 898))
+    .then((pokemon) => {
+      console.log(`Name: ${pokemon.name}`)
+      console.log(`Weight: ${pokemon.weight / 10}kg`)
+      console.log(`Height: ${pokemon.height / 10}m`)
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+displayPokemonData()
